ffbc9e089b4fe14cbdf5165a989eb04f1c6f7cf7
Seyoun Lim <imseyoun@gmail.com>
2019-09-14 16:54:07 +1200

    Adding chart components and mock up bar chart

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionSalesComponent } from './section/section-sales/section-sales.component';
 import { SectionOrdersComponent } from './section/section-orders/section-orders.component';
 import { SectionHealthComponent } from './section/section-health/section-health.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +20,15 @@ import { SectionHealthComponent } from './section/section-health/section-health.
     SidebarComponent,
     SectionSalesComponent,
     SectionOrdersComponent,
-    SectionHealthComponent
+    SectionHealthComponent,
+    BarChartComponent,
+    PieChartComponent,
+    LineChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
