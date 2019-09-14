79603d22627d5f4109aa025d36011f9ba8004783
Seyoun Lim <imseyoun@gmail.com>
2019-09-14 18:35:40 +1200

    Added Server component

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { SectionHealthComponent } from './section/section-health/section-health.
 import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
 import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { LineChartComponent } from './charts/line-chart/line-chart.component';
     SectionHealthComponent,
     BarChartComponent,
     PieChartComponent,
-    LineChartComponent
+    LineChartComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
